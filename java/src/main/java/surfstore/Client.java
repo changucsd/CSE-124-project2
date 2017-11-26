8e99c62242a4f2491492f6f29e00b02b3b08ff0d
He Chang <hechang@Hes-MacBook-Pro.local>
2017-11-25 19:50:51 -0800

    25th

--- java/src/main/java/surfstore/Client.java
+++ java/src/main/java/surfstore/Client.java
@@ -1,8 +1,10 @@
 package surfstore;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import com.google.protobuf.ByteString;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -11,7 +13,7 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import surfstore.SurfStoreBasic.Empty;
-
+import surfstore.SurfStoreBasic.Block;
 
 public final class Client {
     private static final Logger logger = Logger.getLogger(Client.class.getName());
@@ -41,14 +43,55 @@ public final class Client {
         blockChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
     
+    private void ensure(boolean b){
+        if (b == false){
+            throw new RuntimeException("Asssertion failed!");
+        }
+
+    }
+
+    private static Block stringToBlock(String s){
+        Builder builder = Block.newBuilder();
+
+        try{
+            builder.setData(ByteString.copyFrom(s,"UTF-8"));
+        }catch (UnsupportedEncodingException e){
+            throw new  RuntimeException(e);
+        }
+        
+        builder.setHash(HashUtils.sha256(s));
+
+        return builder.build();  //turns the Builder into a Block
+
+    }
 	private void go() {
-		metadataStub.ping(Empty.newBuilder().build());
-        logger.info("Successfully pinged the Metadata server");
+		//metadataStub.ping(Empty.newBuilder().build());
+        //logger.info("Successfully pinged the Metadata server");
         
         blockStub.ping(Empty.newBuilder().build());
         logger.info("Successfully pinged the Blockstore server");
         
+
         // Implement your client here
+
+        Block b1 = stringToBlock("block_01");
+        Block b2 = stringToBlock("block_02");
+
+        ensure(blockStub.hasBlock(b1).getAnswer() == false); // false
+        ensure(blockStub.hasBlock(b2).getAnswer() == false);
+
+        blockStub.storeBlock(b1);
+        ensure(blockStub.hasBlock(b1).getAnswer() == true);
+
+        blockStub.storeBlock(b2);
+        ensure(blockStub.hasBlock(b2).getAnswer() == true);
+
+        Block b1prime = blockStub.getBlock(b1);
+        ensure(b1prime.getHash().euqals(b1.getHash()));
+        ensure(b1.getData().euqals(b1.getData()));
+
+        logger.info("We passed all tests!");
+
 	}
 
     private static Namespace parseArgs(String[] args) {
